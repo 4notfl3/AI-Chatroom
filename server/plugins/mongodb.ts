e1c8b90752ea4781b6acc200e8a43ff182bd5fa0
notfl <2490782554@qq.com>
2026-06-16 11:22:29 +0800

    v2.2

--- server/plugins/mongodb.ts
+++ server/plugins/mongodb.ts
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose'
 
-export default defineNitroPlugin(async () => {
+export default defineNitroPlugin(() => {
   const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/chatroom'
-  try {
-    await mongoose.connect(uri)
-    console.log('MongoDB connected')
-  } catch (e) {
-    console.error('MongoDB connection failed:', e)
-  }
+  mongoose.connect(uri, { serverSelectionTimeoutMS: 3000, connectTimeoutMS: 3000 })
+    .then(() => console.log('MongoDB connected'))
+    .catch(() => console.warn('MongoDB 未连接，消息不会持久化'))
 })
